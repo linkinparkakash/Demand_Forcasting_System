67fbd06e03df3445711d57b6c5e2e0550d76a78a
linkinparkakash <lepump4@hotmail.com>
2023-04-12 05:47:33 +0530

    setup.py added

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open("README.md", 'r', encoding="utf-8") as f:
+    long_description = f.read()
+
+SRC_REPO = "src"
+AUTHOR_NAME = "linkinparkakash"
+REPO_NAME = "Demand_Forcasting_System"
+LIST_OF_REQUIREMENTS = []
+
+setup(name = SRC_REPO, 
+      version="0.0.1",
+      author = AUTHOR_NAME,
+      description= 'This is our first release'
+      long_description= long_description,
+      url = f"https://github.com/{AUTHOR_NAME}/{REPO_NAME}",
+      author_email= "datasciencenini@hotmail.com"
+      packages=SRC_REPO,
+      python_requires = ">=3.10",
+      install_requires = LIST_OF_REQUIREMENTS)
